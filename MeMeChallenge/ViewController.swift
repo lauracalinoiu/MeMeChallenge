31fa1b6f2ddb819605fceb6c899c6f3cb31f8102
Laura C <lauramocanu16@gmail.com>
2015-08-04 22:50:17 +0300

    follow text fields spec

--- MeMeChallenge/ViewController.swift
+++ MeMeChallenge/ViewController.swift
@@ -8,19 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
+    
+    let initialTopText = "TOP"
+    let initialBottomText = "BOTTOM"
     
     @IBOutlet weak var imageView: UIImageView!
-    
     @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var topTextField: UITextField!
     
     override func viewDidLoad(){
         super.viewDidLoad()
+        topTextField.text = initialTopText
+        bottomTextField.text = initialBottomText
+        topTextField.textAlignment = .Center
+        bottomTextField.textAlignment = .Center
     }
     
     override func viewWillAppear(animated: Bool){
         super.viewWillAppear(animated)
         cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+        
+        topTextField.delegate = self
+        bottomTextField.delegate = self
     }
     
     @IBAction func pickClicked(sender: UIBarButtonItem) {
@@ -45,6 +57,22 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
         self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+        if textField == topTextField && textField.text == initialTopText{
+            textField.text = ""
+        }
+        else if textField == bottomTextField && textField.text == initialBottomText{
+            textField.text = ""
+        }
+        
+        return true
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 }
 
